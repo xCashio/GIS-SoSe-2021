db73a2bedd2d84d926f13b7555b9f4e226a867af
xCashio <80816757+xCashio@users.noreply.github.com>
2021-04-22 17:08:10 +0200

    2.1 Start

--- Aufgabe_2.1/script.js
+++ Aufgabe_2.1/script.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("Hello my students");
+/* Aufgabe 1 - Basics */
+function a1() {
+    let x = "Alles";
+    console.log(x);
+    func1();
+    console.log("Logo!");
+}
+a1();
+function func1() {
+    console.log("Klar?");
+}
+/** a) In der Konsole wird nacheinander Alles, Klar?, Logo ausgegeben */
 //# sourceMappingURL=script.js.map
--- Aufgabe_2.1/script.ts
+++ Aufgabe_2.1/script.ts
@@ -1,1 +1,15 @@
-console.log("Hello my students");
+/* Aufgabe 1 - Basics */
+function a1(): void {
+    let x: string = "Alles";
+    console.log(x);
+    func1();
+    console.log("Logo!");
+}
+
+a1();
+
+function func1(): void {
+    console.log("Klar?");
+}
+/** a) In der Konsole wird nacheinander Alles, Klar?, Logo ausgegeben */
+

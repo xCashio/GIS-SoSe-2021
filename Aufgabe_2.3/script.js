da98fc37292c526117fb2ea815ef49bfd052e7f3
xCashio <80816757+xCashio@users.noreply.github.com>
2021-05-06 12:18:51 +0200

    Task 1 with buttons

--- Aufgabe_2.3/script.js
+++ Aufgabe_2.3/script.js
@@ -1,16 +1,43 @@
 "use strict";
-var Aufgabe2_3;
-(function (Aufgabe2_3) {
-    let colors = ["blue", "red", "brown", "yellow", "green", "purple", "pink", "turquoise"];
-    let previousElement = document.body;
-    for (let i = 0; i < 10; i++) {
+var Aufgabe2_3_A1;
+(function (Aufgabe2_3_A1) {
+    /**
+     * Aufgabe 1
+     */
+    /**
+     * Buttons with Eventlistener
+     */
+    let addRecButton = document.createElement("Button");
+    let textRecButton = document.createTextNode("Rectangler");
+    addRecButton.appendChild(textRecButton);
+    document.body.appendChild(addRecButton);
+    addRecButton.id = "RecButtId";
+    let resetWindowButton = document.createElement("Button");
+    let textWinButton = document.createTextNode("Resetter");
+    resetWindowButton.id = "resetButtId";
+    resetWindowButton.appendChild(textWinButton);
+    document.body.appendChild(resetWindowButton);
+    document.getElementById("RecButtId").addEventListener("click", createDivRec);
+    document.getElementById("resetButtId").addEventListener("click", resetWindow);
+    /**
+     * functions for Window resetting and creating Rectangle Divs
+     */
+    function resetWindow() {
+        window.location.reload();
+    }
+    function createDivRec() {
+        let colors = ["blue", "red", "brown", "yellow", "green", "purple", "pink", "turquoise"];
+        let previousElement = document.body;
         let div = document.createElement("div");
-        div.style.height = (50 + Math.floor(Math.random() * 951)).toString() + "px";
-        div.style.width = (50 + Math.floor(Math.random() * 951)).toString() + "px";
+        div.style.height = (50 + Math.floor(Math.random() * 451)).toString() + "px";
+        div.style.width = (50 + Math.floor(Math.random() * 451)).toString() + "px";
         div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)].toString();
         div.style.position = "absolute";
+        div.style.left = (50 + Math.floor(Math.random() * 451)).toString() + "px";
+        div.style.right = (50 + Math.floor(Math.random() * 451)).toString() + "px";
         div.classList.add("rectangle");
         previousElement.appendChild(div);
+        previousElement = div;
     }
-})(Aufgabe2_3 || (Aufgabe2_3 = {}));
+})(Aufgabe2_3_A1 || (Aufgabe2_3_A1 = {}));
 //# sourceMappingURL=script.js.map

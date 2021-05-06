5e8ff9d2c09e69a30305ca8d2c1e0cec7bb36716
xCashio <80816757+xCashio@users.noreply.github.com>
2021-05-06 11:32:42 +0200

    random div recs

--- Aufgabe_2.3/script.js
+++ Aufgabe_2.3/script.js
@@ -1,9 +1,10 @@
-namespace Aufgabe2_3 {
-
-    let colors: String[] = ["blue", "red", "brown", "yellow", "green", "purple", "pink", "turquoise"];
-    let previousElement: HTMLElement = document.body;
-    for (let i: number = 0; i < 10; i++) {
-        let div: HTMLDivElement =  document.createElement("div");
+"use strict";
+var Aufgabe2_3;
+(function (Aufgabe2_3) {
+    let colors = ["blue", "red", "brown", "yellow", "green", "purple", "pink", "turquoise"];
+    let previousElement = document.body;
+    for (let i = 0; i < 10; i++) {
+        let div = document.createElement("div");
         div.style.height = (50 + Math.floor(Math.random() * 951)).toString() + "px";
         div.style.width = (50 + Math.floor(Math.random() * 951)).toString() + "px";
         div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)].toString();
@@ -11,4 +12,5 @@ namespace Aufgabe2_3 {
         div.classList.add("rectangle");
         previousElement.appendChild(div);
     }
-}
+})(Aufgabe2_3 || (Aufgabe2_3 = {}));
+//# sourceMappingURL=script.js.map

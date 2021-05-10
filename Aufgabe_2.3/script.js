a66db00dfb622da8fcfaef67b03045e7292c1cc7
xCashio <80816757+xCashio@users.noreply.github.com>
2021-05-10 20:46:20 +0200

    Task 2
    function for creating divs and creating with fori

--- Aufgabe_2.3/script.js
+++ Aufgabe_2.3/script.js
@@ -2,4 +2,23 @@
 /**
  * Aufgabe 2
  */
+var Aufgabe2_3;
+(function (Aufgabe2_3) {
+    function createWaffleOptions(_waffle) {
+        let div = document.createElement("div");
+        let picture = document.createElement("img");
+        picture.src = _waffle.picture;
+        div.appendChild(picture);
+        let chooseButton = document.createElement("Button");
+        let textChooseButton = document.createTextNode("Choose this size");
+        chooseButton.appendChild(textChooseButton);
+        document.body.appendChild(chooseButton);
+        chooseButton.id = "ChooseButtId";
+        return div;
+    }
+    for (let i = 0; i < waffles.length; i++) {
+        let waffleElements = createWaffleOptions(waffles[i]);
+        document.body.appendChild(waffleElements);
+    }
+})(Aufgabe2_3 || (Aufgabe2_3 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_2.3/script.ts
+++ Aufgabe_2.3/script.ts
@@ -3,6 +3,24 @@
  * Aufgabe 2
  */
 
- namespace Aufgabe2_3 {
-     
- }
+namespace Aufgabe2_3 {
+    function createWaffleOptions(_waffle: Waffle): HTMLElement {
+        let div: HTMLDivElement = document.createElement("div");
+        let picture: HTMLImageElement = document.createElement("img");
+        picture.src = _waffle.picture;
+        div.appendChild(picture);
+        let chooseButton: HTMLElement = document.createElement("Button");
+        let textChooseButton: Text = document.createTextNode("Choose this size");
+        chooseButton.appendChild(textChooseButton);
+        document.body.appendChild(chooseButton);
+        chooseButton.id = "ChooseButtId";
+
+        return div;
+    }
+
+    for (let i: number = 0; i < waffles.length; i++) {
+        let waffleElements: HTMLElement = createWaffleOptions(waffles[i]);
+        document.body.appendChild(waffleElements);
+
+    }
+}

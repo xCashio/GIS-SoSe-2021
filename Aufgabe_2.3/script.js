cca620bba0135473423e1f91a23f5d42df00c5f8
xCashio <80816757+xCashio@users.noreply.github.com>
2021-05-10 21:56:28 +0200

    Task 3 finish
    changed Eventlistener
    console output for chosen waffle

--- Aufgabe_2.3/script.js
+++ Aufgabe_2.3/script.js
@@ -1,24 +1,31 @@
 "use strict";
 /**
- * Aufgabe 2
+ * Aufgabe 3
  */
 var Aufgabe2_3;
 (function (Aufgabe2_3) {
     function createWaffleOptions(_waffle) {
         let div = document.createElement("div");
+        div.style.maxWidth = "150px";
         let picture = document.createElement("img");
         picture.src = _waffle.picture;
+        picture.style.width = "100%";
         div.appendChild(picture);
         let chooseButton = document.createElement("Button");
-        let textChooseButton = document.createTextNode("Choose this size");
+        let textChooseButton = document.createTextNode(_waffle.waffleSort);
         chooseButton.appendChild(textChooseButton);
-        document.body.appendChild(chooseButton);
-        chooseButton.id = "ChooseButtId";
+        chooseButton.addEventListener("click", waffleSave);
+        chooseButton.dataset.waffleSort = _waffle.waffleSort;
+        div.appendChild(chooseButton);
         return div;
     }
     for (let i = 0; i < Aufgabe2_3.waffles.length; i++) {
         let waffleElements = createWaffleOptions(Aufgabe2_3.waffles[i]);
         document.body.appendChild(waffleElements);
+    }
+    function waffleSave(_input) {
+        let output = _input.target;
+        console.log(output.dataset.waffleSort);
     }
 })(Aufgabe2_3 || (Aufgabe2_3 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_2.3/script.ts
+++ Aufgabe_2.3/script.ts
@@ -1,25 +1,31 @@
 
 /**
- * Aufgabe 2
+ * Aufgabe 3
  */
 
 namespace Aufgabe2_3 {
+   
     function createWaffleOptions(_waffle: Waffle): HTMLElement {
         let div: HTMLDivElement = document.createElement("div");
+        div.style.maxWidth = "150px";
         let picture: HTMLImageElement = document.createElement("img");
         picture.src = _waffle.picture;
+        picture.style.width = "100%";
         div.appendChild(picture);
         let chooseButton: HTMLElement = document.createElement("Button");
-        let textChooseButton: Text = document.createTextNode("Choose this size");
+        let textChooseButton: Text = document.createTextNode(_waffle.waffleSort);
         chooseButton.appendChild(textChooseButton);
-        document.body.appendChild(chooseButton);
-        chooseButton.id = "ChooseButtId";
-
+        chooseButton.addEventListener("click", waffleSave);
+        chooseButton.dataset.waffleSort = _waffle.waffleSort;
+        div.appendChild(chooseButton);
         return div;
     }
-
     for (let i: number = 0; i < waffles.length; i++) {
         let waffleElements: HTMLElement = createWaffleOptions(waffles[i]);
         document.body.appendChild(waffleElements);
     }
+    function waffleSave(_input: MouseEvent): void {
+        let output: HTMLElement = <HTMLElement>_input.target;
+        console.log(output.dataset.waffleSort);        
+    }  
 }
